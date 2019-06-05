20a236ec47db178b5d639d3f55095cddcfc6402d
DAVID NOEL <DAVID.NOEL@aexp.com>
2019-06-04 21:33:14 -0400

    writing solidity tests

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,6 +1,6 @@
 /*
     This exercise has been updated to use Solidity version 0.5
-    Breaking changes from 0.4 to 0.5 can be found here: 
+    Breaking changes from 0.4 to 0.5 can be found here:
     https://solidity.readthedocs.io/en/v0.5.0/050-breaking-changes.html
 */
 
@@ -9,13 +9,15 @@ pragma solidity ^0.5.0;
 contract SupplyChain {
 
   /* set owner */
-  address owner;
+  address public owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping(uint => Item) public items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -24,6 +26,12 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
+  enum State {
+    ForSale,
+    Sold,
+    Shipped,
+    Received
+  }
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
@@ -31,16 +39,36 @@ contract SupplyChain {
     if you need help you can ask around :)
     Be sure to add "payable" to addresses that will be handling value transfer
   */
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku */
+    event LogForSale(uint sku);
+    event LogSold(uint sku);
+    event LogShipped(uint sku);
+    event LogReceived(uint sku);
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
+  modifier onlyOwner(){
+    require(msg.sender == owner,"Only owner allowed.");
+    _;
+  }
 
-  modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
+  modifier verifyCaller (address _address) {require (msg.sender == _address,"caller address doesn't match"); _;}
 
-  modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
+  modifier paidEnough(uint _price) {
+    require(msg.value >= _price, "not enough ether sent");
+    _;
+    }
+
   modifier checkValue(uint _sku) {
     //refund them after pay for item (why it is before, _ checks for logic before func)
     _;
@@ -51,20 +79,41 @@ contract SupplyChain {
 
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
-   that the item with the given sku has the state ForSale. 
+   that the item with the given sku has the state ForSale.
    Note that the uninitialized Item.State is 0, which is also the index of the ForSale value,
    so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
    Hint: What item properties will be non-zero when an Item has been added?
    */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale (uint _sku){
+    require(bytes(items[_sku].name).length != 0, "name is needed");
+    require(items[_sku].price > 0, "price is needed");
+    require(items[_sku].seller != address(0), "must have a seller");
+    require(uint(items[_sku].state) == uint(State.ForSale),"item is not for sale");
+    _;
+  }
+
+  modifier sold (uint _sku){
+    require(items[_sku].buyer != address(0), "must have a buyer");
+    require(uint(items[_sku].state) == uint(State.Sold),"item not sold");
+    _;
+  }
+
+  modifier shipped (uint _sku){
+    require(uint(items[_sku].state) == uint(State.Shipped),"item not shipped");
+    _;
+  }
+
+  modifier received (uint _sku){
+    require(uint(items[_sku].state) == uint(State.Received),"item not received");
+    _;
+  }
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+       owner = msg.sender;
+       skuCount = 0;
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
@@ -80,21 +129,31 @@ contract SupplyChain {
     if the buyer paid enough, and check the value after the function is called to make sure the buyer is
     refunded any excess ether sent. Remember to call the event associated with this function!*/
 
-  function buyItem(uint sku)
-    public
-  {}
+  function buyItem(uint sku) public payable forSale(sku) paidEnough(items[sku].price) checkValue(sku)
+  {
+    items[sku].seller.transfer(items[sku].price);
+    items[sku].buyer = msg.sender;
+    items[sku].state = State.Sold;
+    emit LogSold(sku);
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
   function shipItem(uint sku)
-    public
-  {}
+    public sold(sku) verifyCaller(items[sku].seller)
+  {
+    items[sku].state = State.Shipped;
+    emit LogShipped(sku);
+  }
 
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
-    public
-  {}
+    public shipped(sku) verifyCaller(items[sku].buyer)
+  {
+    items[sku].state = State.Received;
+    emit LogReceived(sku);
+  }
 
   /* We have these functions completed so we can run tests, just ignore it :) */
   function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,14 +5,40 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
+    uint public initialBalance = 10 ether;
+    SupplyChain supply = SupplyChain(DeployedAddresses.SupplyChain());
 
     // Test for failing conditions in this contracts:
     // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
 
     // buyItem
 
-    // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
+    function testbuyItem() public {
+
+        // add item
+        bool itemAdded = supply.addItem("book", 1000);
+        Assert.equal(itemAdded,true,"Item not added");
+
+        (string memory name, uint sku, uint price, uint state, address seller, address buyer) = supply.fetchItem(0);
+
+        Assert.equal(price,1000 wei,"not equal");
+        Assert.equal(sku,0,"sku don't match");
+        //supply.buyItem.gas(3000000).value(100)(0);
+        bytes4 methodId = supply.buyItem.selector;
+        bool r;
+        // test for failure if user does not send enough funds
+        (r, ) = address(supply).transfer.value(2000)(abi.encodeWithSelector(methodId,0));
+        //supply.buyItem.gas(3000000).value(10000 wei)(uint(0));
+        //Assert.isFalse(r, "Not enough funds sent.");
+
+        // test for purchasing an item that is not for Sale
+        // first: purchase the item
+        //address(supply).call.gas(3000000).value(0.1 ether)(abi.encodeWithSelector(methodId,0));
+        //(name, sku, price, state, seller, buyer) = supply.fetchItem(0);
+        // check item is purchased
+        //Assert.isTrue(state == 1, "Item not purchased");
+
+    }
 
     // shipItem
 
